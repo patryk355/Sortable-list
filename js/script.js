422456f0b3da116e94bf217bcd7acd3e0277a0e2
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-09-12 20:05:13 +0200

    structure of html, a little css and add variables in js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const list = document.querySelector('#list');
+const checkBtn = document.querySelector('.check');
+
+const values = [
+    4, 15, 222, 450, 777, 958, 999, 1800, 11699, 50000
+];
+
+function randomList(){
+    
+}
