a631c864766790576055c2553e9e2d73738fca8d
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-09-14 01:12:57 +0200

    checkBtn

--- js/script.js
+++ js/script.js
@@ -20,10 +20,35 @@ function randomList() {
             unsortedValues[index] = value;
 
             list.innerHTML += `
-            <li class="list-group-item" data-index="${index}">${unsortedValues[index].value} <i class="fas fa-sort"></i></li>
+            <li class="list-group-item" data-index="${index}" draggable="true">${unsortedValues[index].value} <i class="fas fa-sort"></i></li>
             `;
         })
 
 }
 
 randomList();
+
+function checkList() {
+    const listItems = [...document.querySelectorAll('.list-group-item')];
+
+    values.forEach((value, index) => {
+        if (value === unsortedValues[index].value) {
+            listItems.forEach(li => {
+                if (+li.dataset.index === index) {
+                    li.classList.remove('wrong');
+                    li.classList.add('correct');
+                }
+            });
+        }
+        else if (value !== unsortedValues[index].value) {
+            listItems.forEach(li => {
+                if (+li.dataset.index === index) {
+                    li.classList.remove('correct');
+                    li.classList.add('wrong');
+                }
+            });
+        }
+    })
+}
+
+checkBtn.addEventListener('click', checkList);
