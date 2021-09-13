89c9b14635269fb0aff002be45e2020fe671f1d5
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-09-14 00:12:17 +0200

    generate random list

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ const values = [
     4, 15, 222, 450, 777, 958, 999, 1800, 11699, 50000
 ];
 
-function randomList(){
-    
+const unsortedValues = [];
+
+function randomList() {
+    [...values]
+        .map((value) => ({
+            value: value,
+            method: Math.random()
+        }))
+        .sort((a, b) => {
+            return a.method - b.method;
+        })
+        .map((value, index) => {
+            unsortedValues[index] = value;
+
+            list.innerHTML += `
+            <li class="list-group-item" data-index="${index}">${unsortedValues[index].value} <i class="fas fa-sort"></i></li>
+            `;
+        })
+
 }
+
+randomList();
