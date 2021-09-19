087f1c12906e9beb1712f4898f8dc4b7f274dbd7
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-09-19 15:22:09 +0200

    remove function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-window.onload = function () {
-    //select the thing we wanna drag
-    var mustachio = document.getElementById('gif');
-    //listen to the touchmove event, every time it fires, grab the location of the touch
-    //then assign it to mustachio
-    mustachio.addEventListener('touchmove', function (ev) {
-        //grab the location of the touch
-        var touchLocation = ev.targetTouches[0];
-        //assign mustachio new coordinates based on the touch
-        mustachio.style.left = touchLocation.pageX + 'px';
-        mustachio.style.top = touchLocation.pageY + 'px';
-    })
-    mustachio.addEventListener('touchend', function (ev) {
-        //current mustachio position when dropped
-        var x = parseInt(mustachio.style.left);
-        var y = parseInt(mustachio.style.top);
-        //check to see if that position meets our constraints
-        if (x < 388 || x > 646) {
-            mustachio.style.left = '450px';
-            mustachio.style.top = '175px';
-        }
-        if (y < 100 || y > 356) {
-            mustachio.style.left = '450px';
-            mustachio.style.top = '175px';
-        }
-    })
-}
-
-
 const list = document.querySelector('#list');
 const checkBtn = document.querySelector('.check');
 
